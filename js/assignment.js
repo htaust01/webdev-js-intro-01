16501ed0c29ca07080d7cbb78e4d5bc8189316a1
Hunter Austin <htaust01@gmail.com>
2024-12-18 18:16:38 -0500

    completed assignment

--- js/assignment.js
+++ js/assignment.js
@@ -9,29 +9,29 @@ const submissionBtn = document.getElementById("submission-btn")
 
 // Declare a constant variable named "book" and assign
 // the name of your favorite book to that variable.
-
+const book = "The Hitchhiker's Guide to the Galaxy";
 
 // Declare a mutable variable named "activity" and assign
 // an activity to it.
-
+let activity = "bowling";
 
 
 function updateBookSentence() {
-    // Sets the text inside the element with the id of book answer. 
+    // Sets the text inside the element with the id of book answer.
     // innerText is one of many properties on a HTML element.
-    bookAnswerEl.innerText = book
+    bookAnswerEl.innerText = book;
 }
 
 function updateActivitySentence() {
-    // Update this function 
+    // Update this function
     // (hint: it will be similiar to the previous function)
-
+    activityAnswerEl.innerText = activity;
 }
 
 function render() {
     // Finish writing this function
     updateBookSentence();
-
+    updateActivitySentence();
 }
 
 submissionBtn.addEventListener("click", function () {
--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+const temp = "temp";
+console.log(temp);
+
+let temp2 = "2";
+console.log(temp2);
+
+temp2 = "3";
+console.log(temp2);
+*/
